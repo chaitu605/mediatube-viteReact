b393e06296820b6807ab7ee6aaf992a6a5ca90d9
chaitu605 <chaitanya605@gmail.com>
2024-01-27 19:05:40 +0530

    setup/folderStructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login/login";
+import Register from "./components/Register/Register";
+// import Router from "./routes";
+
 function App() {
   return (
     <>
-      <div></div>
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<Login />} />
+        <Route path="/sign-up" element={<Register />} />
+
+        {/* private routes */}
+      </Routes>
     </>
   );
 }
