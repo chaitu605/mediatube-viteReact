2f81dfa74b7a9be14798e9616e4b5d36a5a543d3
chaitu605 <chaitanya605@gmail.com>
2024-01-27 19:06:47 +0530

    setup/folderStructure

---
+++ src/protectedRoutes.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+// import { Outlet, Navigate } from "react-router-dom";
+// import Nav from "./components/Nav/Nav";
+
+// export default function ProtectedRoutes() {
+//   let auth = { token: false };
+//   return (
+//     <>
+//       <Nav />
+//       <main>{auth.token ? <Outlet /> : <Navigate to={`/login`} />}</main>
+//     </>
+//   );
+// }
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+// import { Navigate, useRoutes } from "react-router-dom";
+// import ProtectedRoutes from "./protectedRoutes";
+// import Home from "./components/Home/home";
+
+// export default function Router() {
+//   const routes = useRoutes([
+//     {
+//       element: <ProtectedRoutes />,
+//       children: [{ path: "/", element: <Home />, index: true }],
+//     },
+//   ]);
+
+//   return routes;
+// }
